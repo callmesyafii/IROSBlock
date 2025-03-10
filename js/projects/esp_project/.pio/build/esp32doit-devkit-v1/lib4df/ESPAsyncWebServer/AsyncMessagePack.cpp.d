0978955a4ee21336862a9381de01701c63fde709
Muhammad Auriel <auriel.muhammadkece@gmail.com>
2025-03-10 11:25:18 +0700

    filesystem fix

--- js/projects/esp_project/.pio/build/esp32doit-devkit-v1/lib4df/ESPAsyncWebServer/AsyncMessagePack.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib4df/ESPAsyncWebServer/AsyncMessagePack.cpp.o: \
- .pio/libdeps/esp32doit-devkit-v1/ESPAsyncWebServer/src/AsyncMessagePack.cpp \
- .pio/libdeps/esp32doit-devkit-v1/ESPAsyncWebServer/src/AsyncMessagePack.h
--- js/projects/esp_project/.pio/build/esp32doit-devkit-v1/src/main.cpp.d
+++
@@ -1,1 +0,0 @@
-.pio/build/esp32doit-devkit-v1/src/main.cpp.o: src/main.cpp
